84ba92db3fee3328e1fd2f0014c60d808f1576d0
manpro <manpro@softleaf.co.kr>
2022-01-27 16:00:20 +0900

    Initial commit

---
+++ src/main/java/com/softleaf/bootprj/BootprjApplication.java
@@ -0,0 +1,13 @@
+package com.softleaf.bootprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootprjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootprjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softleaf/bootprj/BootprjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softleaf.bootprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootprjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
